a1981e4636858673ee74ab65ec4ac32376c3a3ac
Krunal Patel <krunalkachadia@gmail.com>
2022-12-29 18:28:28 -0500

    .

--- cppcode/branch_cumpscost.cpp
+++ cppcode/branch_cumpscost.cpp
@@ -276,7 +276,7 @@ static SCIP_DECL_BRANCHEXECLP(branchExeclpCumpscost)
    if (best_lp_candidate_index == -1)
       use_relpscost = true;
 
-   if (rand() % 10 >= 5)
+   if (rand() % 10 >= 3)
       use_relpscost = true;
 
    if (use_relpscost)
