fb9fb749b8fdfede056591903bd4adc77b4d5a93
Krunal Patel <krunalkachadia@gmail.com>
2023-01-07 11:18:19 -0500

    max restart is a parameter 0,1

--- cppcode/main.cpp
+++ cppcode/main.cpp
@@ -75,6 +75,7 @@ struct BranchingStats
 
 SCIP_RETCODE execmain(int argc, const char **argv)
 {
+    srand(42);
     string meta_file_path = argv[1];
     cout << meta_file_path << endl;
     int pos = meta_file_path.find("datasets");
@@ -187,8 +188,8 @@ SCIP_RETCODE execmain(int argc, const char **argv)
 
         // TODO: use timeout -1
         SCIP_CALL(SCIPsetRealParam(scip, "limits/time", timeout - 2));
-        // SCIP_CALL(SCIPsetIntParam(scip, "presolving/maxrestarts", max_restarts.GetBestValue()));
-        SCIP_CALL(SCIPsetIntParam(scip, "presolving/maxrestarts", 0));
+        SCIP_CALL(SCIPsetIntParam(scip, "presolving/maxrestarts", max_restarts.GetBestValue()));
+        // SCIP_CALL(SCIPsetIntParam(scip, "presolving/maxrestarts", 0));
         SCIP_CALL(SCIPsetIntParam(scip, "separating/maxcuts", max_cuts.GetBestValue()));
         SCIP_CALL(SCIPsetIntParam(scip, "separating/maxcutsroot", max_cuts_root.GetBestValue()));
         // SCIP_CALL(SCIPsetIntParam(scip, "branching/pscost/priority", 40000)); // default 2000
