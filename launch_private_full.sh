cdd73e59295d4cfe8ed83f9fb959b9f30249541b
krooonal <krunalkachadia@gmail.com>
2023-01-10 14:50:17 -0500

    .

---
+++ launch_private_full.sh
@@ -0,0 +1,2 @@
+nohup sh mipcomp.sh datasets/testfiles/magic_series_1.test > logs/full/$1/magic_series_1.txt &
+nohup sh mipcomp.sh datasets/testfiles/magic_series_2.test > logs/full/$1/magic_series_2.txt &
