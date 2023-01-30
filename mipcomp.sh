0daf84cd6f43c7b3cb1aa2e8140d43a951a11c48
Krunal Patel <krunalkachadia@gmail.com>
2023-01-30 11:33:36 -0500

    Conditional compilation

--- mipcomp.sh
+++ mipcomp.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-
+# Old python code.
 #python scipmain.py $1
 
-#cd cppcode/build/
-#make
-#cd ../../
-# ./cppcode/build/cppex $1
+if ! [ -f cppcode/build/cppex ];
+then
+    mkdir -p cppcode/build
+    cd cppcode/build/
+    cmake .. -DSCIP_DIR=/home/patekrun/scipoptsuite-8.0.2/build/scip/; 
+    make
+    cd ../../
+fi
 
-./cppcode/build/basecppex $1
+./cppcode/build/cppex $1
+
+# Command to compute base benchmarks.
+# ./cppcode/build/basecppex $1
